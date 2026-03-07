1f02a0764eba716b536377e848b7ead45eff5626
Mohd Kaif <kaif85994@gmail.com>
2026-03-07 10:35:49 +0530

    naturalnoCheck

---
+++ Class-Work/Selection-Statement/discount.py
@@ -0,0 +1,6 @@
+#program to check is eligble for discount or not
+amount=int(input("Enter amount: "))
+if amount>3000:
+    print("Eligible For discount")
+else :
+    print("Not elgible for discount")
---
+++ Class-Work/Selection-Statement/naturalnumbercheck.py
@@ -0,0 +1,7 @@
+# program to check no is natural or not
+# taking inpur from user
+num=int(input("Enter a num"))
+if num>0:
+    print(num,"is natural no")
+else:
+    print(num,"is not Natural")
