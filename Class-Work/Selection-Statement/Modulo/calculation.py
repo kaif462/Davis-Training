55ca0cd363a1e3ff30f053f11c547175d4b5f7d6
Mohd Kaif <kaif85994@gmail.com>
2026-04-08 10:28:31 +0530

    Add files via upload

---
+++ Class-Work/Selection-Statement/Modulo/calculation.py
@@ -0,0 +1,12 @@
+from numericcalculation import *
+m=10
+n=20
+print("sum of",m,"and",n,"is:",addNumbers(m,n))
+
+print("Diff of",m,"and",n,"is:",calculateDifference(m,n))
+
+print("Product of",m,"and",n,"is:",calculateProduct(m,n))
+
+print("Div of",m,"and",n,"is:",divideNum(m,n))
+
+print("Modulo of",m,"and",n,"is:",calculateModulo(m,n))
---
+++ Class-Work/Selection-Statement/Modulo/numericcalculation.py
@@ -0,0 +1,20 @@
+# function to add two numbers
+def addNumbers(a,b):
+    return a+b
+    #------------------
+# function to calc diff
+def calculateDifference(a,b):
+    return a-b
+# function to calc product
+
+def calculateProduct(a,b):
+    return a*b
+# function to calc divide
+
+def divideNum(a,b):
+    return a/b
+
+# function tp calc modulo
+ 
+def calculateModulo(a,b):
+    return a%b
