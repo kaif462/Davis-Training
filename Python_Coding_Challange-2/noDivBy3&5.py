cdac4bb29938f0eec4117e8d64b5e6a1c5e89e73
Mohd Kaif <kaif85994@gmail.com>
2026-04-09 09:39:23 +0530

    Add files via upload

---
+++ Python_Coding_Challange-2/noDivBy3&5.py
@@ -0,0 +1,7 @@
+# To take input
+n=int(input('Enter number: '))
+#Checks No Is div by 3&5 or not
+if n%3==0 and n%5==0:
+    print('Yes')
+else:
+    print('No')
---
+++ Python_Coding_Challange-2/validate_login_credentials.py
@@ -0,0 +1,9 @@
+# to take user name
+user=input('Enter username: ')
+# to take password
+pass=input('Enter password: ')
+# to Validate user name And Password
+if user=='admin' and pass=='1234':
+    print('Login Successful')
+else:
+    print('Invalid Credentials')
