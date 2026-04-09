e12c7958893c68855fd501c80f8e781e8e0530bb
Mohd Kaif <kaif85994@gmail.com>
2026-04-09 11:25:55 +0530

    Delete Python_Coding_Challange-2/validate_login_credentials.py

--- Python_Coding_Challange-2/validate_login_credentials.py
+++
@@ -1,9 +0,0 @@
-# to take user name
-user=input('Enter username: ')
-# to take password
-pass=input('Enter password: ')
-# to Validate user name And Password
-if user=='admin' and pass=='1234':
-    print('Login Successful')
-else:
-    print('Invalid Credentials')
